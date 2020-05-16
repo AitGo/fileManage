ceef57c7033f57389a74094c50d6f7f13e2004af
liu <ly490099@163.com>
2020-05-16 12:09:58 +0800

    增加主页面界面

--- app/src/main/java/com/yh/filesmanage/view/MainActivity.java
+++ app/src/main/java/com/yh/filesmanage/view/MainActivity.java
@@ -1,21 +1,28 @@
 package com.yh.filesmanage.view;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.yh.filesmanage.R;
 import com.yh.filesmanage.base.BaseFragmentActivity;
-import com.yh.filesmanage.utils.ToastUtils;
-import com.yh.filesmanage.widget.HorizontalSelectedView;
+import com.yh.filesmanage.view.fragment.MainFragment;
 
-import java.util.ArrayList;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseFragmentActivity {
 
-    @BindView(R.id.hsv)
-    HorizontalSelectedView hsv;
+
+    @BindView(R.id.fl_view)
+    FrameLayout flView;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    private MainFragment mMainFragment;
+
 
     @Override
     protected int getLayoutId() {
@@ -24,22 +31,15 @@ public class MainActivity extends BaseFragmentActivity {
 
     @Override
     protected void initView() {
-        ArrayList<String> strings = new ArrayList<>();
-        for(int i = 0;i < 10;i++) {
-            strings.add(i + 1 + "");
-        }
-        hsv.setData(strings);
-        hsv.setOnSelectListener(new HorizontalSelectedView.OnSelectListener() {
-            @Override
-            public void onSelect(String selectStr, int index) {
-                ToastUtils.showLong(selectStr);
-            }
-        });
+        addFragment();
+        hideFragment();
+        showFragment(mMainFragment);
     }
 
     @Override
     protected void initData() {
-
+        fragmentManager = getSupportFragmentManager();
+        transaction = fragmentManager.beginTransaction();
     }
 
     @Override
@@ -47,5 +47,29 @@ public class MainActivity extends BaseFragmentActivity {
         super.onCreate(savedInstanceState);
         // TODO: add setContentView(...) invocation
         ButterKnife.bind(this);
+    }
+
+    public void showFragment(Fragment fragment) {
+        transaction = fragmentManager.beginTransaction();
+        transaction.show(fragment);
+        transaction.commit();
+    }
+
+    public void addFragment() {
+        transaction = fragmentManager.beginTransaction();
+        if (mMainFragment == null) {
+            mMainFragment = new MainFragment();
+            transaction.add(R.id.fl_view, mMainFragment);
+        }
+
+        transaction.commit();
+    }
+
+    public void hideFragment() {
+        transaction = fragmentManager.beginTransaction();
+        if (mMainFragment != null) {
+            transaction.hide(mMainFragment);
+        }
+        transaction.commit();
     }
 }
--- app/src/main/java/com/yh/filesmanage/view/fragment/MainFragment.java
+++ app/src/main/java/com/yh/filesmanage/view/fragment/MainFragment.java
@@ -1,13 +1,58 @@
 package com.yh.filesmanage.view.fragment;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.yh.filesmanage.R;
 import com.yh.filesmanage.base.BaseFragment;
+import com.yh.filesmanage.widget.WheelView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainFragment extends BaseFragment {
+    @BindView(R.id.tv_main_time)
+    TextView tvMainTime;
+    @BindView(R.id.tv_main_week)
+    TextView tvMainWeek;
+    @BindView(R.id.tv_main_date)
+    TextView tvMainDate;
+    @BindView(R.id.wv_main)
+    WheelView wvMain;
+    @BindView(R.id.btn_main_confirm)
+    Button btnMainConfirm;
+    @BindView(R.id.ll_main_setting)
+    LinearLayout llMainSetting;
+    @BindView(R.id.tv_main_server_address)
+    TextView tvMainServerAddress;
+    @BindView(R.id.tv_main_files_setting)
+    TextView tvMainFilesSetting;
+    @BindView(R.id.tv_main_serial_setting)
+    TextView tvMainSerialSetting;
+    @BindView(R.id.tv_main_order_setting)
+    TextView tvMainOrderSetting;
+    @BindView(R.id.tv_main_db_address)
+    TextView tvMainDbAddress;
+    @BindView(R.id.tv_main_param_setting)
+    TextView tvMainParamSetting;
+    @BindView(R.id.ll_main_open)
+    LinearLayout llMainOpen;
+    @BindView(R.id.ll_main_check)
+    LinearLayout llMainCheck;
+    @BindView(R.id.ll_main_select)
+    LinearLayout llMainSelect;
+    @BindView(R.id.ll_main_statis)
+    LinearLayout llMainStatis;
+    @BindView(R.id.ll_main_info)
+    LinearLayout llMainInfo;
+    @BindView(R.id.ll_main_manage)
+    LinearLayout llMainManage;
+
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.fragment_main;
     }
 
     @Override
@@ -18,5 +63,39 @@ public class MainFragment extends BaseFragment {
     @Override
     protected void initData() {
 
+    }
+
+    @OnClick({R.id.btn_main_confirm, R.id.ll_main_setting, R.id.tv_main_server_address, R.id.tv_main_files_setting, R.id.tv_main_serial_setting, R.id.tv_main_order_setting, R.id.tv_main_db_address, R.id.tv_main_param_setting, R.id.ll_main_open, R.id.ll_main_check, R.id.ll_main_select, R.id.ll_main_statis, R.id.ll_main_info, R.id.ll_main_manage})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_main_confirm:
+                break;
+            case R.id.ll_main_setting:
+                break;
+            case R.id.tv_main_server_address:
+                break;
+            case R.id.tv_main_files_setting:
+                break;
+            case R.id.tv_main_serial_setting:
+                break;
+            case R.id.tv_main_order_setting:
+                break;
+            case R.id.tv_main_db_address:
+                break;
+            case R.id.tv_main_param_setting:
+                break;
+            case R.id.ll_main_open:
+                break;
+            case R.id.ll_main_check:
+                break;
+            case R.id.ll_main_select:
+                break;
+            case R.id.ll_main_statis:
+                break;
+            case R.id.ll_main_info:
+                break;
+            case R.id.ll_main_manage:
+                break;
+        }
     }
 }
