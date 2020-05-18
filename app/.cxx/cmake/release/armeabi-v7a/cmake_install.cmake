acb4859b5ce8bae66c9c596ff260f3530d5c737c
liu <ly490099@163.com>
2020-05-18 13:59:20 +0800

    增加qmui

--- app/.cxx/cmake/release/armeabi-v7a/cmake_install.cmake
+++ app/.cxx/cmake/release/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/studioworkspace/FilesManage/app/src/main/cpp
+# Install script for directory: D:/androidProgream/fileManage/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/studioworkspace/FilesManage/app/.cxx/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/androidProgream/fileManage/app/.cxx/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
